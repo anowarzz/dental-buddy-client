d40c0f1ebee348e0db33dab43cd7d6b388d73d47
anowarzz <anowarhosen444@gmail.com>
2022-11-08 10:38:46 +0600

    added login and register page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,70 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Lottie from "lottie-react";
+import login from '../../assets/login-animation.json'
 
 const Login = () => {
-    return (
-        <div>
-            <h3 className='text-center'>Login Features coming soon</h3>
-        </div>
+        return (
+            <div className="hero w-full my-20">
+              <div className="mx-auto hero-content grid md:grid-cols-2 gap-20 flex-col lg:flex-row items-center justify-center">
+                <div className="text-center lg:text-left">
+               
+               <Lottie animationData={login} loop={true}/>
+               
+                </div>
+                <div className="card pt-10 pb-20  border border-gray-200 shadow-md w-9/12">
+                  <h1 className="text-3xl md:text-5xl text-center font-bold">Login</h1>
+                  <form  className="card-body">
+                    <div className="form-control">
+                      <label className="label">
+                        <span className="label-text font-bold">Email</span>
+                      </label>
+                      <input name="email" 
+                        type="text"
+                        placeholder="Your Email"
+                        className="input input-bordered"
+                      />
+                    </div>
+                    <div className="form-control">
+                      <label className="label">
+                        <span className="label-text font-bold">Password</span>
+                      </label>
+                      <input name="password" 
+                        type="password"
+                        placeholder="Your Password"
+                        className="input input-bordered"
+                      />
+                    </div>
+                   
+                    <div className="text-end mt-5">
+                    <label className="">
+                      <Link
+                        to=""
+                        alt=""
+                        className="label-text-alt  text-center"
+                      >
+                        <button  className="hover:link">Forgot password?</button>
+                      </Link>
+                    </label>
+                    </div>
+                    {/* { 
+                      error &&
+                      <p className="text-brightRed font-bold text-center mb-3">{error}</p>
+                    } */}
+                    <div className="form-control mt-6">
+                      <input
+                        className="btn bg-blue-500 border-none"
+                        type="submit"
+                        value="Login"
+                      />
+                    </div>
+                  </form>
+                  
+                  <p className="text-center">New Here? <Link to='/register' className="text-brightRed font-bold hover:link pl-3">
+                 Sign Up </Link> </p>
+                </div>
+              </div>
+            </div>
     );
 };
 
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,9 +1,82 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Lottie from "lottie-react";
+import register from '../../assets/register-animation.json'
 
 const Register = () => {
     return (
         <div>
-            <h2 className='text-center'>Register section coming soon</h2>
+           <div className="hero w-full my-20">
+      <div className="hero-content grid md:grid-cols-2 gap-20 flex-col lg:flex-row items-center justify-center">
+        <div className="text-center lg:text-left">
+          <Lottie animationData={register} loop={true}/>
+        </div>
+        <div className="card pt-10 pb-20 lg:w-9/12 border border-gray-200 shadow-md">
+          <h1 className="text-3xl md:text-5xl text-center font-bold">Sign Up</h1>
+          <form  className="card-body">
+
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-bold">Name</span>
+              </label>
+              <input name='name'  
+                type="text"
+                placeholder="Enter Your Name"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-bold">Email</span>
+              </label>
+              <input name='email' 
+                type="text"
+                placeholder="Enter Your Email"
+                className="input input-bordered" required
+              />
+            </div>
+
+           
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-bold">Password</span>
+              </label>
+              <input name='password' 
+                type="password"
+                placeholder="Type Your Password"
+                className="input input-bordered" required
+              />
+            </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-bold">Confirm Password</span>
+              </label>
+              <input name='confirm' 
+                type="password"
+                placeholder="Confirm Your Password"
+                className="input input-bordered" required
+              />
+            </div>
+            {/* { 
+              error &&
+              <p className="text-brightRed font-bold text-center mb-3">{error}</p>
+            } */}
+            <div className="form-control mt-6">
+              <input
+                className="btn bg-blue-500 border-none"
+                type="submit"
+                value="Sign Up"
+              />
+            </div>
+          </form>
+          <p className="text-center">Already Have An Account ? <Link to='/login' className="text-brightRed font-bold hover:link pl-3">
+         Login</Link> </p>
+        </div>
+      </div>
+    </div>
         </div>
     );
 };
