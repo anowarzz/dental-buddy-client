0778c7718ad35f25f578b571c4b562da745211d3
anowarzz <anowarhosen444@gmail.com>
2022-11-11 18:35:45 +0600

    home section design complete

--- src/components/Pages/Home/HomeSection.js
+++ src/components/Pages/Home/HomeSection.js
@@ -6,14 +6,14 @@ const HomeSection = () => {
         <div>
            <section className="text-gray-800">
 	<div className="container flex flex-col-reverse mx-auto lg:flex-row">
-		<div className="flex flex-col px-6 py-8 space-y-6 rounded-sm sm:p-8 lg:p-12 lg:w-1/2 xl:w-2/5 bg-violet-600 text-gray-50">
+		<div className="flex flex-col px-6 py-8 space-y-6 rounded-sm sm:p-8 lg:p-12 lg:w-1/2 xl:w-2/5 ">
 			<div className="flex space-x-2 sm:space-x-4">
 				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="flex-shrink-0 w-6 h-6">
 					<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"></path>
 				</svg>
 				<div className="space-y-2">
 					<p className="text-lg font-medium leading-snug">Online Booking</p>
-					<p className="leading-snug">Praesentium ea et neque distinctio quas eius repudiandae quaerat obcaecati voluptatem similique!</p>
+					<p className="leading-snug">You can book your appointment online easily that can save your time and make it easier for you to reach your dentist when needed.</p>
 				</div>
 			</div>
 			<div className="flex space-x-2 sm:space-x-4">
@@ -22,7 +22,7 @@ const HomeSection = () => {
 				</svg>
 				<div className="space-y-2">
 					<p className="text-lg font-medium leading-snug">Well Cared</p>
-					<p className="leading-snug">Praesentium ea et neque distinctio quas eius repudiandae quaerat obcaecati voluptatem similique!</p>
+					<p className="leading-snug">I am committed to solve your all dental problems with maximum care and bring a great smile for you.</p>
 				</div>
 			</div>
 			<div className="flex space-x-2 sm:space-x-4">
@@ -31,7 +31,7 @@ const HomeSection = () => {
 				</svg>
 				<div className="space-y-2">
 					<p className="text-lg font-medium leading-snug">Emergency Support</p>
-					<p className="leading-snug">Praesentium ea et neque distinctio quas eius repudiandae quaerat obcaecati voluptatem similique!</p>
+					<p className="leading-snug">You can have emergency support and medicine in  when you need that type of support.</p>
 				</div>
 			</div>
 			<div className="flex space-x-2 sm:space-x-4">
@@ -40,7 +40,7 @@ const HomeSection = () => {
 				</svg>
 				<div className="space-y-2">
 					<p className="text-lg font-medium leading-snug">First Consultation For Free</p>
-					<p className="leading-snug">Praesentium ea et neque distinctio quas eius repudiandae quaerat obcaecati voluptatem similique!</p>
+					<p className="leading-snug">Your first consultation and checkup for your dental problems is always free. So visit and get a regular checkup for free </p>
 				</div>
 			</div>
 			<div className="flex space-x-2 sm:space-x-4">
@@ -48,8 +48,8 @@ const HomeSection = () => {
 					<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"></path>
 				</svg>
 				<div className="space-y-2">
-					<p className="text-lg font-medium leading-snug">Emergency Support</p>
-					<p className="leading-snug">Praesentium ea et neque distinctio quas eius repudiandae quaerat obcaecati voluptatem similique!</p>
+					<p className="text-lg font-medium leading-snug">Modern Dental Technologies</p>
+					<p className="leading-snug">I have all the modern Technologies an machine that is require to solve all your dental problems in a modern way</p>
 				</div>
 			</div>
 		</div>
